9d5fa9db120fce4334a6f00726c8e4a99b89b7ce
Igor Santos <igorsantos07@gmail.com>
2016-07-18 14:19:24 -0300

    Using locally-ignored database file as Codeception does not support in-memory dbs for now

--- tests/_envs/phalcon/app/config/config.php
+++ tests/_envs/phalcon/app/config/config.php
@@ -3,8 +3,8 @@
         'adapter' => Phalcon\Db\Adapter\Pdo\Sqlite::class,
 
         // see bug https://github.com/Codeception/Codeception/issues/3319
-//        'dbname'  => dirname(dirname(dirname(dirname(__DIR__)))) . '/_data/test.db',
-        'dbname'  => dirname(dirname(dirname(dirname(dirname(__DIR__))))) . '/:memory:',
+        'dbname'  => dirname(dirname(dirname(dirname(__DIR__)))) . '/_data/base.db',
+//        'dbname'  => dirname(dirname(dirname(dirname(dirname(__DIR__))))) . '/:memory:',
 //        'dbname'  => ':memory:',
     ],
 ];

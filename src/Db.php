7ca9bf0212ed3cfa38d61018211b5c75dfd2c830
Igor Santos <igorsantos07@gmail.com>
2016-07-19 19:00:59 -0300

    partial implementation of correct watch/choose/use methods; added some stub methods to be implemented later

--- src/Db.php
+++ src/Db.php
@@ -22,13 +22,23 @@ class Db extends Beanstalk
     /** @var \Phalcon\Db\Adapter\Pdo */
     protected $connection;
 
-    protected $activeTube = 'default';
+    /**
+     * Where to put jobs.
+     * @var string
+     */
+    protected $using = 'default';
+
+    /**
+     * Where to get jobs from.
+     * @var array
+     */
+    protected $watching = ['default'];
 
     /** Time to run (aka timeout) */
 //    const OPT_TTR      = 'ttr';
     /** How long to wait before this job becomes available */
     const OPT_DELAY    = 'delay';
-    const OPT_PRIORITY = 'priority'; //TODO: test me under JobTest
+    const OPT_PRIORITY = 'priority';
     const OPT_TUBE     = 'tube';
 
     const OPTIONS = [
@@ -65,53 +75,92 @@ class Db extends Beanstalk
 
     /**
      * Reserves a job in the queue
-     * @param mixed $timeout
+     * @param int $timeout How long to wait while pooling for a new job
+     * @param int $pool    How frequently to pool for a new job (in seconds).
      * @return bool|\Phalcon\Queue\Db\Job
      */
-    public function reserve($timeout = null)
+    public function reserve($timeout = 0, $pool = 1)
     {
     }
 
     /**
-     * Change the active tube. The default tube is "default".
+     * Change the active tube to put jobs on. The default tube is "default".
      * @param string $tube
      * @return string
-     * @see \Phalcon\Queue\Db::getActiveTube()
-     * @see \Phalcon\Queue\Db::watch()
+     * @see \Phalcon\Queue\Db::using()
      */
     public function choose($tube)
     {
-        return $this->activeTube = (string)$tube;
+        return $this->using = (string)$tube;
     }
 
     /**
-     * Change the active tube. The default tube is "default".
-     * @param string $tube
+     * Change what tubes to watch for when getting jobs. The default value is "default".
+     * @param string|array $tube Name of one or more tubes to watch
+     * @param bool $replace If the current watch list should be replaced for this one, or just include these
      * @return string
-     * @see \Phalcon\Queue\Db::getActiveTube()
-     * @see \Phalcon\Queue\Db::choose()
+     * @see \Phalcon\Queue\Db::watching()
      */
-    public function watch($tube) { return $this->choose($tube); }
+    public function watch($tube, $replace = false) {
+        if ($replace) {
+            $this->watching = (array)$tube;
+        } else {
+            $this->watching = array_unique(array_merge($this->watching, (array)$tube));
+        }
+        return $this->watching;
+    }
 
     /**
-     * Returns what tube is currently active.
+     * Removes a tube from the list of watched tubes.
+     * Can't be used to un-watch the last tube, so in this case, it will silently ignore the request.
+     * @param string $tube
+     * @return array The final list of watched tubes
+     */
+    public function ignore($tube)
+    {
+        if (sizeof($this->watching) > 1) {
+            $this->watching = array_filter($this->watching, function($v) use ($tube) { return $v != $tube; });
+        }
+        return $this->watching;
+    }
+
+    /**
+     * Returns what tube is currently active to put jobs on.
      * @return string
      * @see \Phalcon\Queue\Db::choose()
+     * @see \Phalcon\Queue\Db::chosen()
+     */
+    public function using() { return $this->using; }
+
+
+    /**
+     * Returns what tube is currently active to put jobs on.
+     * Alias of {@link \Phalcon\Queue\Db::using()}.
+     * @return string
+     * @see \Phalcon\Queue\Db::choose()
+     * @see \Phalcon\Queue\Db::using()
+     */
+    public function chosen() { return $this->using(); }
+
+    /**
+     * Returns what tube(s) will be used to get jobs from.
+     * @return array
      * @see \Phalcon\Queue\Db::watch()
      */
-    public function getActiveTube() { return $this->activeTube; }
+    public function watching() { return $this->watching; }
 
     /**
      * Get stats of the open tubes.
      * Each entry (keyed by tube) contains the following keys, pointing to the number of corresponding jobs on the
      * table:
-     *   - active (waiting for being worked on)
      *   - buried (failed)
      *   - delayed (going to be ready for work only later)
+     *   - ready (waiting for being worked on)
      *   - reserved (being worked on)
      *   - total (sum of all)
      * @param string $filterTube Return only data regarding a given tube
      * @return array[]
+     * @todo missing "urgent" count
      */
     public function stats($filterTube = null)
     {
@@ -119,12 +168,12 @@ class Db extends Beanstalk
             ->columns([
                 'tube',
                 'COUNT(*) AS total',
-                'buried',
-                'reserved',
-                'delay >= :timestamp: AS delayed',
+                Job::ST_BURIED,
+                Job::ST_RESERVED,
+                'delay >= :timestamp: AS '.Job::ST_DELAYED,
             ])
             ->bind(['timestamp' => time()])
-            ->groupBy(['tube', 'buried', 'reserved', 'delayed'])
+            ->groupBy(['tube', Job::ST_BURIED, Job::ST_RESERVED, Job::ST_DELAYED])
             ->orderBy('tube');
 
         if ($filterTube) {
@@ -134,11 +183,11 @@ class Db extends Beanstalk
         $result = $query->execute();
 
         $structure = [
-            'active'   => 0,
-            'buried'   => 0,
-            'delayed'  => 0,
-            'reserved' => 0,
-            'total'    => 0,
+            Job::ST_BURIED   => 0,
+            Job::ST_DELAYED  => 0,
+            Job::ST_READY    => 0,
+            Job::ST_RESERVED => 0,
+            'total'          => 0,
         ];
         $stats     = (!$filterTube) ? ['all' => $structure] : [];
         foreach ($result->toArray() as $entry) {
@@ -151,7 +200,7 @@ class Db extends Beanstalk
             }
 
             $entry  = array_filter($entry); //leaves only the status that has a value
-            $status = $entry ? array_keys($entry)[0] : 'active'; //if there's no key, it's the count for active jobs
+            $status = $entry ? array_keys($entry)[0] : Job::ST_READY; //if there's no key, it's the count for ready jobs
             $stats[$tube][$status] += $total;
             $stats[$tube]['total'] += $total;
             if (!$filterTube) {
@@ -166,9 +215,9 @@ class Db extends Beanstalk
     /**
      * Get stats of a tube. By default, gets from the currently active tube.
      * Returns an array with the following keys:
-     *   - active (waiting for being worked on)
      *   - buried (failed)
      *   - delayed (going to be ready for work only later)
+     *   - ready (waiting for being worked on)
      *   - reserved (being worked on)
      *   - total (sum of all)
      * @param string $tube
@@ -176,7 +225,7 @@ class Db extends Beanstalk
      */
     public function statsTube($tube = null)
     {
-        return $this->stats($tube?: $this->activeTube);
+        return $this->stats($tube?: $this->using);
     }
 
     /**
@@ -196,8 +245,17 @@ class Db extends Beanstalk
     }
 
     /**
+     * Peeks into a specific job.
+     * @param int $id
+     * @return null|\Phalcon\Queue\Db\Job
+     */
+    public function peek($id)
+    {
+    }
+
+    /**
      * Inspect the next ready job.
-     * @return bool|\Phalcon\Queue\Db\Job
+     * @return null|\Phalcon\Queue\Db\Job
      */
     public function peekReady()
     {
@@ -205,7 +263,7 @@ class Db extends Beanstalk
             'conditions' => 'tube = :tube: AND delay < :timestamp:',
             'order'      => 'id ASC',
             'bind'       => [
-                'tube'      => $this->activeTube,
+                'tube'      => $this->using,
                 'timestamp' => time()
             ],
         ]);
@@ -223,6 +281,24 @@ class Db extends Beanstalk
      */
     public function peekBuried()
     {
+    }
+
+    /**
+     * Inspect the next delayed job.
+     * @return null|\Phalcon\Queue\Db\Job
+     */
+    public function peekDelayed()
+    {
+    }
+
+    /**
+     * Kicks back into the queue $number buried jobs (at least one).
+     * @param int $number
+     * @return int total of actually kicked jobs
+     */
+    public function kick($number = 1)
+    {
+
     }
 
     public function connect() { throw new BadMethod('"connect" is not a valid method in DB queues.'); }

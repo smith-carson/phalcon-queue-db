21d33572549456e93c6755dc49c8d976ee8932b7
Igor Santos <igorsantos07@gmail.com>
2016-07-25 14:58:19 -0400

    Applied fixes from StyleCI

--- src/Db/Job.php
+++ src/Db/Job.php
@@ -5,7 +5,7 @@ use Phalcon\Queue\Db\Job\Stats;
 use Phalcon\Queue\Db\Model as JobModel;
 
 /**
- * Job from the DB backend
+ * Job from the DB backend.
  */
 class Job extends \Phalcon\Queue\Beanstalk\Job
 {
@@ -16,7 +16,7 @@ class Job extends \Phalcon\Queue\Beanstalk\Job
     protected $model;
 
     /**
-     * Used while the instance is still valid but the job was deleted
+     * Used while the instance is still valid but the job was deleted.
      * @var bool
      */
     protected $deleted = false;
@@ -58,7 +58,7 @@ class Job extends \Phalcon\Queue\Beanstalk\Job
 
     public function getId()
     {
-        return (int)$this->_id;
+        return (int) $this->_id;
     }
 
     public function getState()
@@ -98,7 +98,7 @@ class Job extends \Phalcon\Queue\Beanstalk\Job
     {
         $data = [
             'reserved' => 0,
-            'delay'    => ($delay > 0)? time() + $delay : 0,
+            'delay'    => ($delay > 0) ? time() + $delay : 0,
         ];
         if ($priority) {
             $data['priority'] = $priority;
@@ -130,11 +130,11 @@ class Job extends \Phalcon\Queue\Beanstalk\Job
     {
         $payload = [];
         switch ($this->getState()) {
-            case self::ST_BURIED: $payload = ['buried' => 0]; break;
+            case self::ST_BURIED: $payload  = ['buried' => 0]; break;
             case self::ST_DELAYED: $payload = ['delay' => 0]; break; //FIXME: missing tests for kicking a delayed job!
         }
 
-        return $payload? $this->model->update($payload) : true;
+        return $payload ? $this->model->update($payload) : true;
     }
 
     /**
@@ -152,9 +152,9 @@ class Job extends \Phalcon\Queue\Beanstalk\Job
             'age'           => time() - $model->created_at,
             'state'         => $this->getState(),
             'tube'          => $model->tube,
-            'delay'         => ($delay > 0)? $delay : 0,
-            'delayed_until' => (int)$model->delay,
-            'priority'      => (int)$model->priority,
+            'delay'         => ($delay > 0) ? $delay : 0,
+            'delayed_until' => (int) $model->delay,
+            'priority'      => (int) $model->priority,
             'priority_text' => $model->priorityText(),
         ]);
     }
